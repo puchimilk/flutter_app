62c14eb7aad72fad0f2e739da56c153ab1c01570
puchimilk <puchimilk1539@gmail.com>
2021-04-18 18:25:52 +0900

    master

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ GlobalKey key = GlobalKey();
 OverlayEntry overlayEntry;
 
 class _MyHomePageState extends State<MyHomePage> {
-  final weekday = ['San', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  final weekday = ['SAN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
   void initState() {
     if (overlayEntry != null) {
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void dispose() {
-    // overlayEntry.remove();
+    overlayEntry.remove();
     super.dispose();
   }
 
@@ -58,7 +58,11 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Center(
+          child: Text(
+            '0000年00月',
+          ),
+        ),
       ),
       body: Container(
         child: Column(
@@ -67,20 +71,26 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Row(
                 children: List.generate(7, (i) {
                   var weekcolor = TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold);
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  );
                   switch (i) {
                     case 0:
                       weekcolor = TextStyle(
-                          color: Colors.red, fontWeight: FontWeight.bold);
-                      break;
+                        color: Colors.redAccent,
+                        fontWeight: FontWeight.bold,
+                      );
+                    break;
                     case 6:
                       weekcolor = TextStyle(
-                          color: Colors.blue, fontWeight: FontWeight.bold);
-                      break;
+                        color: Colors.blueAccent,
+                        fontWeight: FontWeight.bold,
+                      );
+                    break;
                   }
                   return Expanded(
                     child: Container(
-                      height: 28,
+                      height: 32,
                       child: Center(
                         child: Text(
                           weekday[i],
@@ -111,32 +121,55 @@ class _MyHomePageState extends State<MyHomePage> {
       // ページの初期位置
       initialPage: 0,
     );
-    final date = DateTime(1800, 01, 01);
-    final firstDate = DateTime(date.year, date.month, 1);
-    final lastDate = DateTime(date.year, date.month + 1, 0);
-    final lastMonthEndDay = DateTime(date.year, date.month - 1, 0);
-    final startDay = firstDate.weekday;
-    var weekdayPro = firstDate.weekday;
-    if (weekdayPro == 7) {
-      weekdayPro = 0;
+    
+    // 今日の日付
+    final today = DateTime(1900, 01, 01);
+    // 先月の最終日
+    final lastDayOfLastMonth = DateTime(today.year, today.month, 0);
+    // 来月の初日
+    final firstDayOfNextMonth = DateTime(today.year, today.month + 1, 1);
+    // 今月の初日
+    final firstDay = DateTime(today.year, today.month, 1);
+    // 今月の最終日
+    final lastDay = DateTime(today.year, today.month + 1, 0);
+    // 曜日を取得
+    var todayWeekday = today.weekday;
+    // 曜日が'7'だった場合'0'へ変更
+    if (todayWeekday == 7) {
+      todayWeekday = 0;
     }
-    var oneMonthDay = 35;
-    if ((weekdayPro + lastDate.day) > 35) {
-      oneMonthDay = 42;
+    // 空のリストを作成
+    var dateList = [];
+    // 先月の日付を追加
+    for (var i = 0; i < firstDay.weekday; i++) {
+      if (lastDayOfLastMonth.weekday == 6) {
+        dateList.add(null);
+      } else {
+        dateList.add(lastDayOfLastMonth.day - i);
+      }
     }
-    var weekNum = 5;
-    if (oneMonthDay == 42) {
-      weekNum = 6;
+    var date = List.from(dateList.reversed);
+    // 今月の日付を追加
+    for (var i = firstDay.day; i <= lastDay.day; i++) {
+      date.add(i);
     }
-    final b = List.generate(
-        oneMonthDay,
-        (i) => DateTime(
-            firstDate.year, firstDate.month, firstDate.day + (i - weekdayPro)));
-    final c = List.generate(oneMonthDay, (i) => i).map((int i) {
-      final d = b[i].day;
-      return d;
-    }).toList();
-
+    // 来月の日付を追加
+    for (var i = 0;
+        i < firstDayOfNextMonth.day + (6 - firstDayOfNextMonth.weekday);
+        i++) {
+      if (firstDayOfNextMonth.weekday == 0) {
+        date.add(null);
+      } else {
+        date.add(firstDayOfNextMonth.day + i);
+      }
+    }
+    var weekNumber;
+    if (date.length == 35) {
+      weekNumber = 5;
+    } else {
+      weekNumber = 6;
+    }
+    
     return Stack(
       children: [
         Positioned(
@@ -151,13 +184,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 return Material(
                   color: Colors.transparent,
                   child: Wrap(
-                    children: List.generate(oneMonthDay, (i) {
+                    children: List.generate(date.length, (i) {
                       return SizedBox(
                         width: size.width / 7,
-                        height: size.height / weekNum,
+                        height: size.height / weekNumber,
                         child: Container(
                           child: Text(
-                            '${c[i]}',
+                            '${date[i]}',
+                            textAlign: TextAlign.center,
                             style: TextStyle(
                               fontWeight: FontWeight.bold,
                             ),
