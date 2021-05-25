b948e9bb5a39fdc4fc2703ead0cdbeaa3b88355d
puchimilk <puchimilk1539@gmail.com>
2021-05-25 22:40:35 +0900

    master

--- lib/src/month_calendar_view.dart
+++ lib/src/month_calendar_view.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 
 class MonthCalendarView extends StatefulWidget {
@@ -36,6 +35,95 @@ class _MonthCalendarView extends State<MonthCalendarView> {
   Widget build(BuildContext context) {
     final PageController controller = PageController();
     
+    return Expanded(
+      child: PageView.builder(
+        controller: controller,
+        itemBuilder: (BuildContext context, int index) {
+          final date = [];
+          final base = DateTime(1900, 01, 01);
+          // 今月の初日
+          final first = DateTime(base.year, base.month + index, 1);
+          var firstWeekdayday = first.weekday;
+          if (firstWeekdayday == 7) {
+            firstWeekdayday = 0;
+          }
+          // 今月の最終日
+          final last = DateTime(first.year, first.month + 1, 0);
+          var lastWeekday = last.weekday;
+          if (lastWeekday == 7) {
+            lastWeekday = 0;
+          }
+          // 先月の最終日
+          final lastMonth = DateTime(base.year, base.month + index, 0);
+          var lastMonthWeekday = lastMonth.weekday;
+          if (lastMonthWeekday == 7) {
+            lastMonthWeekday = 0;
+          }
+          // 来月の初日
+          final nextMonth = DateTime(base.year, base.month + 1 + index, 1);
+          var nextMonthWeekday = nextMonth.weekday;
+          if (nextMonthWeekday == 7) {
+            nextMonthWeekday = 0;
+          }
+          // 先月の日にち
+          if (firstWeekdayday != 0) {
+            for (var i = 0; i < firstWeekdayday; i++) {
+              var reverse = lastMonth.day - lastMonthWeekday;
+              date.add(reverse + i);
+            }
+          }
+          // 今月の日にち
+          for (var i = 0; i < last.day; i++) {
+            date.add(first.day + i);
+          }
+          // 来月の日にち
+          if (lastWeekday != 6) {
+            for (var i = 0; i < (6 - lastWeekday); i++) {
+              date.add(nextMonth.day + i);
+            }
+          }
+          
+          return LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              Size size = Size(constraints.maxWidth, constraints.maxHeight);
+              var height = 5;
+              if (date.length == 42) {
+                height = 6;
+              }
+              
+              return Wrap(
+                children: List.generate(date.length, (int index) {
+                  return SizedBox(
+                    width: size.width / 7,
+                    height: size.height / height,
+                    child: GestureDetector(
+                      child: Container(
+                        color: _selectedIndex == index
+                            ? Colors.black12
+                            : Colors.transparent,
+                        child: Text(
+                          '${date[index]}',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        _onSelected(index);
+                        _counter(index);
+                      },
+                    ),
+                  );
+                }),
+              );
+            },
+          );
+        },
+        itemCount: 1200,
+      ),
+    );
+    
+    /*
     return Expanded(
       child: PageView.builder(
         controller: controller,
@@ -129,6 +217,7 @@ class _MonthCalendarView extends State<MonthCalendarView> {
         itemCount: 1200,
       ),
     );
+    */
   }
   
   Future<void> _showCalendarDialog(BuildContext context) async {
