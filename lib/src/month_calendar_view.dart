86c6a3590b400ddfe577bcf8b22f169e21c16164
puchimilk <puchimilk1539@gmail.com>
2021-05-25 21:36:47 +0900

    master

--- lib/src/month_calendar_view.dart
+++ lib/src/month_calendar_view.dart
@@ -29,7 +29,6 @@ class _MonthCalendarView extends State<MonthCalendarView> {
       if (_list.length == 2 && _list[0] == _list[1]) {
         _showCalendarDialog(context);
       }
-      print(_list);
     });
   }
   
@@ -41,27 +40,74 @@ class _MonthCalendarView extends State<MonthCalendarView> {
       child: PageView.builder(
         controller: controller,
         itemBuilder: (BuildContext context, int index) {
+          final date = [];
+          final base = DateTime(1900, 01, 01);
+          // 今月の初日
+          final first = DateTime(base.year, base.month + index, 1);
+          var firstWeekdayday = first.weekday;
+          if (firstWeekdayday == 7) {
+            firstWeekdayday = 0;
+          }
+          // 今月の最終日
+          final last = DateTime(first.year, first.month + 1, 0);
+          var lastWeekday = last.weekday;
+          if (lastWeekday == 7) {
+            lastWeekday = 0;
+          }
+          // 先月の最終日
+          final lastMonth = DateTime(base.year, base.month + index, 0);
+          var lastMonthWeekday = lastMonth.weekday;
+          if (lastMonthWeekday == 7) {
+            lastMonthWeekday = 0;
+          }
+          // 来月の初日
+          final nextMonth = DateTime(base.year, base.month + 1 + index, 1);
+          var nextMonthWeekday = nextMonth.weekday;
+          if (nextMonthWeekday == 7) {
+            nextMonthWeekday = 0;
+          }
+          // 先月の日にち
+          if (firstWeekdayday != 0) {
+            for (var i = 0; i < firstWeekdayday; i++) {
+              var reverse = lastMonth.day - lastMonthWeekday;
+              date.add(reverse + i);
+            }
+          }
+          // 今月の日にち
+          for (var i = 0; i < last.day; i++) {
+            date.add(first.day + i);
+          }
+          // 来月の日にち
+          if (lastWeekday != 6) {
+            for (var i = 0; i < (6 - lastWeekday); i++) {
+              date.add(nextMonth.day + i);
+            }
+          }
+          var length = 35;
+          var hei = 5;
+          if ((last.day + lastWeekday) > 35) {
+            length = 42;
+            hei = 6;
+          }
+          
           return Container(
-            child: Column(
-              children: [
-                Expanded(
-                  child: LayoutBuilder(
-                    builder: (BuildContext context, BoxConstraints constraints) {
-                      Size size = Size(constraints.maxWidth, constraints.maxHeight);
-                      
-                      return Wrap(
-                        children: List.generate(35, (index) {
-                          return SizedBox(
-                            width: size.width / 7,
-                            height: size.height / 5,
+            child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                Size size = Size(constraints.maxWidth, constraints.maxHeight);
+                
+                return Wrap(
+                  children: List.generate(length, (index) {
+                    Color changeColor = _selectedIndex == index ? Colors.black12 : Colors.transparent;
+                    
+                    return SizedBox(
+                      width: size.width / 7,
+                      height: size.height / hei,
                             child: GestureDetector(
                               child: Container(
                                 padding: EdgeInsets.all(4),
-                                color: _selectedIndex == index
-                                    ? Colors.black12
-                                    : Colors.transparent,
+                                color: changeColor,
                                 child: Text(
-                                  '$index',
+                                  '${date[index]}',
                                   style: TextStyle(
                                     fontSize: 16,
                                   ),
@@ -77,9 +123,7 @@ class _MonthCalendarView extends State<MonthCalendarView> {
                       );
                     },
                   ),
-                ),
-              ],
-            ),
+            
           );
         },
         itemCount: 1200,
@@ -145,21 +189,13 @@ class _MonthCalendarView extends State<MonthCalendarView> {
 }
 
 /*
-class MonthCalendarView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final PageController controller = PageController();
-    
-    return PageView.builder(
-      controller: controller,
-      itemBuilder: (BuildContext context, int index) {
         final date = [];
         final base = DateTime(1900, 01, 01);
         // 今月の初日
         final first = DateTime(base.year, base.month + index, 1);
-        var firstWeekday = first.weekday;
-        if (firstWeekday == 7) {
-          firstWeekday = 0;
+        var firstWeekdayday = first.weekday;
+        if (firstWeekdayday == 7) {
+          firstWeekdayday = 0;
         }
         // 今月の最終日
         final last = DateTime(first.year, first.month + 1, 0);
@@ -180,8 +216,8 @@ class MonthCalendarView extends StatelessWidget {
           nextMonthWeekday = 0;
         }
         // 先月の日にち
-        if (firstWeekday != 0) {
-          for (var i = 0; i < firstWeekday; i++) {
+        if (firstWeekdayday != 0) {
+          for (var i = 0; i < firstWeekdayday; i++) {
             var reverse = lastMonth.day - lastMonthWeekday;
             date.add(reverse + i);
           }
@@ -246,14 +282,4 @@ class MonthCalendarView extends StatelessWidget {
       itemCount: 1200,
     );
   }
-  
-  Future<void> _showCalendarDialog(BuildContext context) async {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return Container();
-      },
-    );
-  }
-}
 */
