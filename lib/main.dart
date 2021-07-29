f2d6de7cfd30162d41733566e1b724f5abe4e2bc
puchimilk <puchimilk1539@gmail.com>
2021-07-29 11:57:29 +0900

    master

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/src/month_calendar_view.dart';
 
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
+// import 'package:path/path.dart';
+// import 'package:sqflite/sqflite.dart';
 
-void main() async {
+void main() {
   runApp(MyApp());
-}
-
-class Date {
-  Date({
-    this.id,
-    this.fulldate,
-    this.year,
-    this.month,
-    this.day,
-    this.weekday,
-  });
-
-  final int id;
-  final String fulldate;
-  final int year;
-  final int month;
-  final int day;
-  final int weekday;
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'fulldate': fulldate,
-      'year': year,
-      'month': month,
-      'day': day,
-      'weekday': weekday,
-    };
-  }
-
-  @override
-  String toString() {
-    return 'Date{id: $id, fulldate: $fulldate, year: $year, month: $month, day: $day, weekday: $weekday}';
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -81,23 +48,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    DateTime startDate;
-    DateTime endDate;
-    startDate = DateTime.now();
-    endDate = startDate.add(Duration(days: 365));
-    var milliseconds = endDate.millisecondsSinceEpoch - startDate.millisecondsSinceEpoch;
-    var list = [];
-    var st = 'A';
-    for (var i = 0; i < milliseconds; i++) {
-      list.add(st);
-    }
-    print('print: ${list.length}');
-    
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(),
+      body: Container(
+        child: MonthCalendarView(),
+      ),
     );
   }
 }
