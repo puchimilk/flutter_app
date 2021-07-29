e221df464b15f056ba345784213fbfac5acf3e90
puchimilk <puchimilk1539@gmail.com>
2021-07-29 11:51:46 +0900

    master

--- lib/main.dart
+++ lib/main.dart
@@ -5,78 +5,12 @@ import 'package:sqflite/sqflite.dart';
 
 void main() async {
   runApp(MyApp());
-
-  WidgetsFlutterBinding.ensureInitialized();
-
-  Future<Database> database = openDatabase(
-    join(await getDatabasesPath(), 'calendar_database.db'),
-    version: 1,
-    onCreate: (Database db, int version) {
-      return db.execute(
-        'CREATE TABLE Date (id INTEGER PRIMARY KEY, year INTEGER, month INTEGER, day INTEGER, weekday INTEGER)',
-      );
-    }
-  );
-
-  Future<void> insert(Date date) async {
-    final Database db = await database;
-    await db.insert(
-      'Date',
-      date.toMap(),
-      conflictAlgorithm: ConflictAlgorithm.replace,
-    );
-  }
-
-  var startDate = DateTime(1899, 12, 31);
-  var endDate = DateTime(2100, 1, 2);
-  // 73051
-  /*
-  for (var i = 0; i < 10; i++) {
-    var a;
-    var date = DateTime(1899, 12, 31);
-    var b = DateTime(date.year, date.month, date.day + i);
-    a = Date(
-      id: i,
-      year: b.year,
-      month: b.month,
-      day: b.day,
-      weekday: b.weekday,
-    );
-    await insert(a);
-  }
-  */
-
-  Future<List<Date>> getDate() async {
-    final Database db = await database;
-    final List<Map<String, dynamic>> maps = await db.query('Date');
-    return List.generate(maps.length, (i) {
-      return Date(
-        id: maps[i]['id'],
-        year: maps[i]['year'],
-        month: maps[i]['month'],
-        day: maps[i]['day'],
-        weekday: maps[i]['weekday'],
-      );
-    });
-  }
-
-  Future<void> update(Date date) async {
-    final Database db = await database;
-    await db.update(
-      'Date',
-      date.toMap(),
-      where: 'id = ?',
-      whereArgs: [date.id],
-      conflictAlgorithm: ConflictAlgorithm.fail,
-    );
-  }
-
-  //print('print: ${await getDate()}');
 }
 
 class Date {
   Date({
     this.id,
+    this.fulldate,
     this.year,
     this.month,
     this.day,
@@ -84,6 +18,7 @@ class Date {
   });
 
   final int id;
+  final String fulldate;
   final int year;
   final int month;
   final int day;
@@ -92,6 +27,7 @@ class Date {
   Map<String, dynamic> toMap() {
     return {
       'id': id,
+      'fulldate': fulldate,
       'year': year,
       'month': month,
       'day': day,
@@ -101,7 +37,7 @@ class Date {
 
   @override
   String toString() {
-    return 'Date{id: $id, year: $year, month: $month, day: $day, weekday: $weekday}';
+    return 'Date{id: $id, fulldate: $fulldate, year: $year, month: $month, day: $day, weekday: $weekday}';
   }
 }
 
