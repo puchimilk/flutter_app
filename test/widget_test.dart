0cd56359942d31eb0b6b9f23f87eccd1e5077338
puchimilk <puchimilk1539@gmail.com>
2021-12-10 23:28:43 +0900

    master

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,11 +6,9 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_app/monthly_page.dart';
+import 'package:flutter_app/main.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_app/main.dart';
 
 void main() {
   testWidgets('main.dart', (WidgetTester tester) async {
