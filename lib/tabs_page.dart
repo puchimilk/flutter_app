f3588f9a1f466cf2f1486d7cc1281899a9c3addc
puchimilk <puchimilk1539@gmail.com>
2021-12-10 19:55:42 +0900

    master

---
+++ lib/tabs_page.dart
@@ -0,0 +1,92 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter/material.dart';
+
+class TabsPage extends StatefulWidget {
+  const TabsPage({Key? key}) : super(key: key);
+
+  static const String routeName = '/tab';
+
+  @override
+  State<StatefulWidget> createState() => _TabsPageState();
+}
+
+class _TabsPageState extends State<TabsPage>
+    with
+        SingleTickerProviderStateMixin,
+        // ignore: prefer_mixin
+        RouteAware {
+  final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+
+  late final TabController _controller = TabController(
+    vsync: this,
+    length: tabs.length,
+    initialIndex: selectedIndex,
+  );
+  int selectedIndex = 0;
+
+  final List<Tab> tabs = <Tab>[
+    const Tab(text: 'LEFT'),
+    const Tab(text: 'RIGHT'),
+  ];
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      setState(() {
+        if (selectedIndex != _controller.index) {
+          selectedIndex = _controller.index;
+          _sendCurrentTabToAnalytics();
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        bottom: TabBar(
+          controller: _controller,
+          tabs: tabs,
+        ),
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: tabs.map((Tab tab) {
+          return Center(child: Text(tab.text!));
+        }).toList(),
+      ),
+    );
+  }
+
+  @override
+  void didPush() {
+    _sendCurrentTabToAnalytics();
+  }
+
+  @override
+  void didPopNext() {
+    _sendCurrentTabToAnalytics();
+  }
+
+  void _sendCurrentTabToAnalytics() {
+    analytics.setCurrentScreen(
+      screenName: '${TabsPage.routeName}/tab$selectedIndex',
+    );
+  }
+}
