13f8d881d5498c999a46e6cb5c66f37408b3de64
puchimilk <puchimilk1539@gmail.com>
2021-12-10 23:22:11 +0900

    master

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_app/monthly_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_app/main.dart';
@@ -14,7 +15,7 @@ import 'package:flutter_app/main.dart';
 void main() {
   testWidgets('main.dart', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(ProviderScope(child: MyApp()));
 
     // Verify that our counter starts at 0.
     //expect(find.text('0'), findsOneWidget);
