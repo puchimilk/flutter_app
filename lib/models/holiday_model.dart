5790a748397529c8e15bdf8f68211b9b7df978bd
puchimilk <puchimilk1539@gmail.com>
2021-12-15 15:52:01 +0900

    master

---
+++ lib/models/holiday_model.dart
@@ -0,0 +1,11 @@
+class HolidayModel {
+  const HolidayModel({
+    required this.month,
+    required this.date,
+    required this.name,
+  });
+  
+  final int month;
+  final int date;
+  final String name;
+}
