1d9c541ecc04e3855a667ec7fc5933ff40672072
puchimilk <puchimilk1539@gmail.com>
2021-12-20 10:53:58 +0900

    master

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,6 +11,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('MyApp()', (WidgetTester tester) async {
-    await tester.pumpWidget(ProviderScope(child: MyApp()));
+    await tester.pumpWidget(const ProviderScope(child: const MyApp()));
   });
 }
