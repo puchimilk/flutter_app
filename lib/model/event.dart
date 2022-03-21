bb6db533b1c2ca6fc018dbad1a53b43a8a2cd648
puchimilk <puchimilk1539@gmail.com>
2022-03-22 01:32:00 +0900

    model追加・削除

---
+++ lib/model/event.dart
@@ -0,0 +1,37 @@
+class Event {
+  const Event({
+    this.id,
+    required this.summary,
+    this.description,
+    required this.created,
+    this.updated,
+    this.location,
+    required this.colorId,
+    required this.startDate,
+    this.endDate,
+    required this.isAllDay,
+  });
+
+  final int? id;
+  final String summary;
+  final String? description;
+  final DateTime created;
+  final DateTime? updated;
+  final String? location;
+  final int colorId;
+  final DateTime startDate;
+  final DateTime? endDate;
+  final bool isAllDay;
+
+  Map<String, dynamic> toMap() => {
+    'summary': summary,
+    'description': description,
+    'created': created.toString(),
+    'updated': updated.toString(),
+    'location': location,
+    'colorId': colorId,
+    'startDate': startDate.toString(),
+    'endDate': endDate.toString(),
+    'isAllDay': isAllDay ? 1 : 0,
+  };
+}
---
+++ lib/model/todo.dart
@@ -0,0 +1,19 @@
+class Todo {
+  const Todo({
+    this.id,
+    required this.title,
+    required this.updated,
+    this.note,
+  });
+
+  final int? id;
+  final String title;
+  final DateTime updated;
+  final String? note;
+
+  Map<String, dynamic> toMap() => {
+    'title': title,
+    'updated': updated.toString(),
+    'note': note,
+  };
+}
